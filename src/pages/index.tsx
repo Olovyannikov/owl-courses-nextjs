b3f3765b79886925928a264f739d23aa6fd36cfc
Ilya Olovyannikov <olovyannikov_barista@icloud.com>
2021-11-17 18:16:13 +0300

    Add server rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,35 @@
 import {withLayout} from "../client/app/layout/Layout";
 import {useState} from "react";
 import {Title, Button, Rating} from "@/components/index";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {MenuItem} from "../client/types/menu.interface";
 
-const Index = (): JSX.Element => {
-    const [rating, setRating] = useState<number>(4);
+const Index = ({menu}: IndexProps): JSX.Element => {
 
     return (
-        <>123</>
+
+        <ul>
+            {menu.map(item => <li key={item._id.secondCategory}>{item._id.secondCategory}</li>)}
+        </ul>
     )
 }
 
 export default withLayout(Index);
+
+export const getStaticProps: GetStaticProps<IndexProps> = async () => {
+    const firstCategory = 0;
+    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+        {firstCategory});
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    }
+}
+
+interface IndexProps extends Record<string, unknown> {
+    menu: MenuItem[];
+    firstCategory: number;
+}
